8a3a6336fcf614028c01dce8d8caf14f3101c98b
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-06-02 20:01:59 -0300

    Fixing the assets load error

--- cgsite/config/application.rb
+++ cgsite/config/application.rb
@@ -9,9 +9,14 @@ Bundler.require(*Rails.groups)
 module Cgsite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.enabled = true
+    config.assets.version = '1.0'
     config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Brasilia'
+    config.beginning_of_week = :sunday
   end
 end
